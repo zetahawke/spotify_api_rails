371ab3a6f1e3d063cfd1c3a02b6c67c2c1e6af95
michel <michel.szinavel@gmail.com>
2017-04-06 18:41:41 -0300

    adding sprockets and assets pipeline config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'csv'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +22,16 @@ module SpotifyStats
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.encoding = 'utf-8'
+    config.time_zone = 'Buenos Aires'
+    config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :en]
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.autoload_paths << "#{Rails.root}/lib"
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('vendor','assets')
   end
 end
