4815be9f5e022932772ac60b43b3c222f968ea49
michel <michel.szinavel@gmail.com>
2017-04-06 19:09:07 -0300

    Adding validation for dont repeat the creation of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def self.register_user(username)
     founded_user = RSpotify::User.find(username)
-    User.create(username: founded_user.display_name, follows: founded_user.followers['total'])
+    User.create(username: founded_user.display_name, follows: founded_user.followers['total']) if User.find_by(username: founded_user.display_name).blank?
   rescue StandardError => e
     puts e.message
   end
